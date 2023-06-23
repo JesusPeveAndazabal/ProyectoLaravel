2c1510edb083ae1a8a7e807a3a71236acf14ffcc
Jesus Peve Andazabal <jesusmauriciopeveandazabal@gmail.com>
2023-06-23 00:29:42 -0500

    Prueba de cards de monitor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,269 @@
 @extends('layouts.app')
 
 @section('content')
+
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('AVANCE DE MONITOR') }}</div>
 
-                <div class="card-body">
+                <<!-- div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
+</div>
+
+<div class="row m-3">
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+</div>
+
+<div class="row m-3">
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
+    <div class="col-md-2">
+        <div class="card">
+            <div class="card-header text-center text-light bg-dark">
+                EMPAQUETADOR 1
+            </div>
+            <div class="card-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col"> <button type="button" class="btn btn-success mb-3" style ="font-size:1.5em">4KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em">160</button><br></div>
+                        <div class="w-100"></div>
+                        <div class="col"><button type="button" class="btn btn-danger mb-2" style ="font-size:1.5em">12KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >80</button><br></div>
+                        <div class="col"><button type="button" class="btn btn-secondary mb-2" style ="font-size:1.5em">16KG</button></div>
+                        <div class="col"><button type="button" class="btn btn-outline-secondary mb-5" style ="font-size:1.5em" >0</button><br></div>
+                    </div>
+                </div>
+            </div>
+        </div> 
+    </div><!-- Fin del col-md -->
 </div>
 @endsection
